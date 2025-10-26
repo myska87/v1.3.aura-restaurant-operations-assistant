55520e8939d04d2a95978f5024ce49dcf04095ba
Mihail Dicusar <mihaildicusar@gmail.com>
2025-10-26 19:56:07 +0000

    Fix Corrupted pages.config.js File

--- src/pages.config.js
+++ src/pages.config.js
@@ -114,9 +114,9 @@ const pages = [
   { name: 'DataManagement', path: '/data-management', file: 'DataManagement.jsx', component: DataManagement, public: false },
   { name: 'BackupSettings', path: '/backup-settings', file: 'BackupSettings.jsx', component: BackupSettings, public: false },
   { name: 'NotFound', path: '/not-found', file: 'NotFound.jsx', component: NotFound, public: false },
-  { name: 'Menu', path: '/menu', file: 'Menu.jsx', component: Menu, public: false },
+  { name: 'Menu', path: '/menu', file: 'Menu.jsx', component: Menu, public: true },
   { name: 'AllergyTable', path: '/allergy-table', file: 'AllergyTable.jsx', component: AllergyTable, public: false },
-  { name: 'EmailLog', path: '/email-log', file: 'EmailLog.jsx', component: EmailLog, public: false },
+  { name: 'EmailLog', path: '/email-log', file: 'EmailLog.jsx', component: EmailLog, public: false }
 ];
 
 export { pages, Layout };
