ae196bc2a35cce543262d9c6238abd4268e0aa90
Mihail Dicusar <mihaildicusar@gmail.com>
2025-10-30 01:58:08 +0000

    File changes

--- src/pages/SOPViewer.jsx
+++ src/pages/SOPViewer.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
@@ -16,7 +17,8 @@ import {
   Wrench,
   BookOpen,
   Edit,
-  FileText
+  FileText,
+  Target
 } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
